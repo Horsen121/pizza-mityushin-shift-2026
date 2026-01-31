12f1be56a184a66a2969a92c832ccedc2602ed75
Horsen121 <vova.mitychin@mail.ru>
2026-01-31 08:34:12 +0700

    feat: make di layer

--- app/src/main/java/com/example/pizza_mityushin_shift_2026/app/app/App.kt
+++ app/src/main/java/com/example/pizza_mityushin_shift_2026/app/app/App.kt
@@ -3,13 +3,13 @@ package com.example.pizza_mityushin_shift_2026.app.app
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import com.example.card.di.pizzaCardModule
 import com.example.main.di.pizzaCatalogModule
 import com.example.network.networkModule
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.mp.KoinPlatform.getKoin
 
 class App: Application(), ImageLoaderFactory {
 
@@ -24,11 +24,12 @@ class App: Application(), ImageLoaderFactory {
 
             modules(
                 networkModule,
-                pizzaCatalogModule
+                pizzaCatalogModule,
+                pizzaCardModule
             )
         }
     }
     override fun newImageLoader(): ImageLoader {
-        return getKoin().get<ImageLoader>() //imageLoader
+        return imageLoader
     }
 }
---
+++ feature/pizza-card/src/main/java/com/example/card/di/PizzaCardModule.kt
@@ -0,0 +1,24 @@
+package com.example.card.di
+
+import com.example.card.data.PizzaCardApi
+import com.example.card.data.converter.PizzaCardItemConverter
+import com.example.card.data.repository.PizzaCardRepositoryImpl
+import com.example.card.domain.entity.PizzaCardItem
+import com.example.card.domain.repository.PizzaCardRepository
+import com.example.card.domain.usecase.GetPizzaCardUseCase
+import com.example.card.presentation.PizzaCardViewModel
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val pizzaCardModule = module {
+    single { get<Retrofit>().create(PizzaCardApi::class.java) }
+    singleOf(::PizzaCardRepositoryImpl) { bind<PizzaCardRepository>() }
+    factoryOf(::PizzaCardItem)
+    factoryOf(::GetPizzaCardUseCase)
+    factoryOf(::PizzaCardItemConverter)
+    viewModelOf(::PizzaCardViewModel)
+}
